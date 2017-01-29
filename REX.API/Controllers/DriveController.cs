a0f88650df5a5f48fed2e77a7813b1c594ffdc2a
tiennsloit <tiennsloit@gmail.com>
2017-01-29 00:30:04 -0800

    Update Values for google api with oauth without success

--- REX.API/Controllers/DriveController.cs
+++ REX.API/Controllers/DriveController.cs
@@ -1,5 +1,7 @@
 ﻿using Google.Apis.Drive.v2;
 using Google.Apis.Drive.v2.Data;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
 using Google.Apis.Services;
 using Newtonsoft.Json;
 using REX.Core;
@@ -12,6 +14,10 @@ using System.Net.Http;
 using System.Web.Hosting;
 using System.Web.Http;
 using System.Web.Mvc;
+using Google.Apis.Auth.OAuth2;
+using System.Threading;
+using System.IO;
+using Google.Apis.Util.Store;
 
 namespace REX.API.Controllers
 {
@@ -23,9 +29,67 @@ namespace REX.API.Controllers
             return new string[] { "value1", "value2" };
         }
 
+        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        static string ApplicationName = "Other client 1";
+        //here is how to generate .json key https://developers.google.com/sheets/api/quickstart/dotnet
         public string GetValue()
         {
-            return "";
+            //IList<Object> obj = new List<Object>();
+            //obj.Add("A2");
+            //obj.Add("B2");
+            //IList<IList<Object>> values = new List<IList<Object>>();
+            //values.Add(obj);
+
+            //SpreadsheetsResource.ValuesResource.AppendRequest request =
+            //        service.Spreadsheets.Values.Append(new ValueRange() { Values = values }, spreadsheetId, range);
+            //request.InsertDataOption = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.INSERTROWS;
+            //request.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.RAW;
+            //var response = request.Execute();
+            var tss = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream(HostingEnvironment.MapPath("/Key/client_secret.json"), FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/sheets.googleapis.com-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(HostingEnvironment.MapPath("/Key"), true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+
+            var services = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            String range = "Sheet1!AD3:AD3";
+            //var file = services.Spreadsheets.Values.Get(StringHelper.GetAppSettingValueOrDefault("DriveFileId", ""), range).Execute();
+            //var filePath = "~/Drive//" + "excel" + DateTime.Now.Ticks.ToString() + ".xlsx";
+           
+            var t =
+                    services.Spreadsheets.Values.Get(StringHelper.GetAppSettingValueOrDefault("DriveFileId", ""), range);
+
+            // Prints the names and majors of students in a sample spreadsheet:
+            // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+            var response = t.Execute();
+            IList<IList<Object>> values = response.Values;
+            var vr = new Google.Apis.Sheets.v4.Data.ValueRange();
+            vr.Values = values;
+            values[0][0] = "y";
+
+            var e = services.Spreadsheets.Values.Update(vr, StringHelper.GetAppSettingValueOrDefault("DriveFileId", ""), range);
+            e.Execute();
+            return values[0][0].ToString();
         }
 
         // GET api/<controller>/5
@@ -36,7 +100,7 @@ namespace REX.API.Controllers
                 ApiKey = StringHelper.GetAppSettingValueOrDefault("DriveAPIKey", ""),  // from https://console.developers.google.com (Public API access)
                 ApplicationName = "Drive API Sample",
             });
-            File file = services.Files.Get(StringHelper.GetAppSettingValueOrDefault("DriveFileId", "")).Execute();
+            var file = services.Files.Get(StringHelper.GetAppSettingValueOrDefault("DriveFileId", "")).Execute();
             var filePath = "~/Drive//" + "excel" + DateTime.Now.Ticks.ToString() + ".xlsx";
             foreach (var link in file.ExportLinks)
             {
