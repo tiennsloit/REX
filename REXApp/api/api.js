02ba504e6537854cc1f82ad1df9cd1422c8e207d
tien nguyen song <tiennsloit@gmail.com>
2017-02-24 01:50:46 -0800

    APP IOS: fix issue with https so that the ios can call web api

--- REXApp/api/api.js
+++ REXApp/api/api.js
@@ -1,48 +1,48 @@
 var api = {
     getContacts() {
 
-        var url = 'http://rexwebapi.azurewebsites.net/api/contact';
+        var url = 'https://rexwebapi.azurewebsites.net/api/contact';
         return fetch(url).then((res) => res.json());
     },
 
     getOrders(contactId){
        
-        var url = 'http://rexwebapi.azurewebsites.net/getOrders/' + contactId;
+        var url = 'https://rexwebapi.azurewebsites.net/getOrders/' + contactId;
         return fetch(url).then((res) => res.json());
     },
 
     getOrder(id){
        
-        var url = 'http://rexwebapi.azurewebsites.net/api/order/' + id;
+        var url = 'https://rexwebapi.azurewebsites.net/api/order/' + id;
         return fetch(url).then((res) => res.json());
     },
 
     getOrderDefaultNewContact() {
-        var url = 'http://rexwebapi.azurewebsites.net/getorderByDefault/2';
+        var url = 'https://rexwebapi.azurewebsites.net/getorderByDefault/2';
         return fetch(url).then((res) => res.json());
     },
 
     getOrderDefaultExistingContact(contactId) {
        
-        var url = 'http://rexwebapi.azurewebsites.net/getorderByDefault/2' + '/' + contactId;
+        var url = 'https://rexwebapi.azurewebsites.net/getorderByDefault/2' + '/' + contactId;
         return fetch(url).then((res) => res.json());
     },
 
     getAllListItems() {
-        var url = 'http://rexwebapi.azurewebsites.net/api/list';
+        var url = 'https://rexwebapi.azurewebsites.net/api/list';
         return fetch(url).then((res) => res.json());
     },
 
     deleteOrder(id)
     {
-        var url = 'http://rexwebapi.azurewebsites.net/api/order/' + id;
+        var url = 'https://rexwebapi.azurewebsites.net/api/order/' + id;
         return fetch(url, {
             method: 'DELETE'
         }).then((res) => res.json());
     },
     finishOrder(id)
     {
-        var url = 'http://rexwebapi.azurewebsites.net/finishOrder/' + id;
+        var url = 'https://rexwebapi.azurewebsites.net/finishOrder/' + id;
         return fetch(url, {
             method: 'PUT'
         }).then((res) => {
@@ -51,7 +51,7 @@ var api = {
 
     createOrder(order) {
         
-        var url = 'http://rexwebapi.azurewebsites.net/api/order/postorder';
+        var url = 'https://rexwebapi.azurewebsites.net/api/order/postorder';
         return fetch(url, {
             method: 'POST',
             headers: {
@@ -64,7 +64,7 @@ var api = {
     
     updateOrder(order) {
         
-        var url = 'http://rexwebapi.azurewebsites.net/api/order/putOrder';
+        var url = 'https://rexwebapi.azurewebsites.net/api/order/putOrder';
         return fetch(url, {
             method: 'PUT',
             headers: {
--- REXApp/index.ios.js
+++ REXApp/index.ios.js
@@ -3,51 +3,34 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
 import React, { Component } from 'react';
+import ReactNative from 'react-native';
+import DatePicker from './controls/date-picker.android.js';
+import NavigatorMain from './components/navigator-main';
+
 import {
   AppRegistry,
   StyleSheet,
+  View,
   Text,
-  View
+  Picker,
+  Navigator
 } from 'react-native';
 
 export default class REXApp extends Component {
+
   render() {
+    
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorMain/>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  picker: {
+    width: 100,
+  }
 });
 
 AppRegistry.registerComponent('REXApp', () => REXApp);
